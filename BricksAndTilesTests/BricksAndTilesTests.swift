0eeb2fdc7b3d827225e323ae19cf6ceef0ef2a7b
mugbug <zaronipedro@outlook.com>
2020-10-17 09:31:04 -0300

    Remove unused files and fix names

--- BricksAndTilesTests/BricksAndTilesTests.swift
+++ BricksAndTilesTests/BricksAndTilesTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import BricksAndTiles
 
-class TableViewFactoryTests: XCTestCase {
+class BricksAndTilesTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
